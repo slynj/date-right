ac5b9e8a090bbd5cff2f88bf11581887e924e568
Lyn <38800761+slynj@users.noreply.github.com>
2023-11-25 04:03:13 -0500

    location field

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def testing():
+    userLocationInput = request.form['user']
+    partnerLocationInput = request.form['partner']
+
+    midpoint = (int(userLocationInput) + int(partnerLocationInput)) / 2
+
+    return str(midpoint)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
